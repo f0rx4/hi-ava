09acbac93164ffa064d97cd47976ed42bea88ee1
iRouzo <rename.umar123@gmail.com>
2020-05-04 22:55:15 +0700

    modified:   assets/quotes.js 	modified:   assets/variables.js 	modified:   config.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,8 @@
 // prettier-ignore
 export default [
     { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "antjedtta_", quotes: "Hi NVTSR, Kangen kamu deh serius, corona selesai ketemuan lah ya hehe" },
+    { author: "standinshd", quotes: "Hey! Kamu yang suka banget mukul mukulin saya" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "standinshd", quotes: "I love you." },
