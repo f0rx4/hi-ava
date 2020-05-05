685b050858af07eb65900631ee1533cdebef83ca
iRouzo <rename.umar123@gmail.com>
2020-05-05 18:48:43 +0700

    modified:   assets/quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,12 +1,12 @@
 // prettier-ignore
 export default [
 //    { author: "Umr", quotes: "Whatever happens, keep breathing" },
-    { author: "Umr", quotes: "Hi NVTSR, Kangen kamu deh serius, corona selesai ketemuan lah ya hehe" },
+//    { author: "Umr", quotes: "Hi NVTSR, Kangen kamu deh serius, corona selesai ketemuan lah ya hehe" },
     { author: "Umr", quotes: "Hey! Kamu yang suka banget mukul mukulin saya" },
     { author: "Umr", quotes: "I love you." },
     { author: "Umr", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "Umr", quotes: "I love you." },
     { author: "Umr", quotes: "I'm sorry for loving you." },
     { author: "Umr", quotes: "I love you." },
-    { author: "Umr", quotes: "kamu yang selalu bikin saya senyum." },
+//    { author: "Umr", quotes: "kamu yang selalu bikin saya senyum." },
 ];
