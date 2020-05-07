4a1f205ac5918c12e1759db07dacb69866c58611
iRouzo <rename.umar123@gmail.com>
2020-05-07 17:39:21 +0700

    modified:   assets/quotes.js 	modified:   assets/variables.js 	modified:   pages/index.vue

--- assets/quotes.js
+++ assets/quotes.js
@@ -3,10 +3,10 @@ export default [
 //    { author: "Umr", quotes: "Whatever happens, keep breathing" },
 //    { author: "Umr", quotes: "Hi NVTSR, Kangen kamu deh serius, corona selesai ketemuan lah ya hehe" },
     { author: "Umr", quotes: "Hey! Kamu yang suka banget mukul mukulin saya" },
-    { author: "Umr", quotes: "I love you." },
+    { author: "Nvt", quotes: "I love you." },
     { author: "Umr", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "Umr", quotes: "I love you." },
+    { author: "Nvt", quotes: "I love you." },
     { author: "Umr", quotes: "I'm sorry for loving you." },
-    { author: "Umr", quotes: "I love you." },
+    { author: "Nvt", quotes: "I love you." },
 //    { author: "Umr", quotes: "kamu yang selalu bikin saya senyum." },
 ];
--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Nvt", "Kamu", "Nvtkuu"],
+  nicknames: ["Nvt", "Neng", "Neneg", "Nvtsr"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
